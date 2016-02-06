045b9b8f7d8aa3347fc46bed83f1f621b71386a5
Tarun Chaudhry <tarunc@users.noreply.github.com>
2016-02-05 16:57:42 -0800

    added js

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 A simple implementation of immutable records for JavaScript.
 
 The goal of this implementation is to have great performance dealing with
-records with a small, fixed set of attributes.
+records with a small, fixed set of attributes. [See here](https://medium.com/outsystems-engineering/javascript-and-immutability-how-fast-is-fast-enough-27790cda4e9e#.4xf0eiq4m) for a performance comparision with Facebook's [immutable-js](https://facebook.github.io/immutable-js/).
 
-If you want to try the code but don't feel like using TypeScript, you can
-use the [online compiler][typescript-playground] to convert it.
-
-  [typescript-playground]: http://www.typescriptlang.org/Playground
+This is the JavaScript version of the [TypeScript](https://github.com/OutSystems/immutable-records) version for use with npm.
---
+++ src/immutable-records.js
@@ -0,0 +1,52 @@
+/**
+ * Used to generate different name for dynamically generated record
+ * classes so V8 understands how to optimize them a little better.
+ */
+var recordGenNumber = 0;
+/**
+ * Dynamically creates a class/constructor for a record, given some
+ * specification of the 'default' for the record, containing all the fields.
+ *
+ * The set of fields in the record is fixed, which means you can't append
+ * a new attribute later on to an instance.
+ *
+ * Usage example:
+ *
+ *     var UserRec = defineRecordClass({name: 'anonymous', age: 20});
+ *     var user1 = new UserRec({name: 'John', age: 21})
+ *     user1.get('age');          // 21
+ *     user1.set('age', 25);      // UserRec({name: 'John', age: 25})
+ *     user1.get('age');          // 21
+ *     var user2 = new UserRec(); // UserRec({name: 'anonymous', age: 20})
+ */
+function defineRecordClass(defaults) {
+    var keys = Object.keys(defaults);
+    var ctor = ("function ImmutableRecord" + recordGenNumber + "(o) { this._ = o || defaults }\n") +
+        ("function R" + recordGenNumber + "(o) {\n");
+    keys.forEach(function (key) { return ctor += "this." + key + " = o." + key + ";\n"; });
+    ctor += "}\n";
+    // build the body of the 'get' function dynamically
+    // using a switch that gets each attribute by direct reference
+    var getBody = "switch(key){\n";
+    keys.forEach(function (key) { getBody += "case \"" + key + "\": return this._." + key + ";\n"; });
+    getBody += "}";
+    // build the body of the 'set' function dynamically
+    // using a direct clone and a reflection set
+    var setBody = "var clone = new R" + recordGenNumber + "(this._);\n";
+    setBody += "switch (key) {\n";
+    keys.forEach(function (key) { setBody += "case \"" + key + "\": clone." + key + "=value;break;"; });
+    setBody += "}\n";
+    setBody += "return new ImmutableRecord" + recordGenNumber + "(clone);\n";
+    var toJSBody = "return {\n";
+    toJSBody += keys.map(function (key) { return (key + ": this._." + key); }).join(",\n");
+    toJSBody += "\n};";
+    var complete = ctor +
+        ("ImmutableRecord" + recordGenNumber + ".prototype.get = function(key){" + getBody + "};\n") +
+        ("ImmutableRecord" + recordGenNumber + ".prototype.set = function(key,value){" + setBody + "};\n") +
+        ("ImmutableRecord" + recordGenNumber + ".prototype.toJS = function(){" + toJSBody + "};\n") +
+        ("return ImmutableRecord" + recordGenNumber + ";");
+    recordGenNumber++;
+    return (new Function("defaults", complete))(defaults);
+}
+
+module.exports.Record = defineRecordClass;
